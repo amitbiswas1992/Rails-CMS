72669654404fbd5c68ea15b1a90850d09b0da930
Amit Biswas <biswas1992@hotmail.com>
2017-03-03 03:30:36 -0500

    worked with migration and fix the migration problem

--- db/migrate/20170227062723_create_users.rb
+++ db/migrate/20170227062723_create_users.rb
@@ -17,3 +17,16 @@ class CreateUsers < ActiveRecord::Migration[5.0]
 drop_table :users
   end
 end
+
+
+# Migration Method
+# create_table :table_name , option do |t|
+#t.column "field_name" :string :limit => 25
+#t.string "password" :default => '', :null => false
+
+# column migration
+
+# add_column(table,colomn,type, option)
+# change_column(table,column,type,optio)
+# remove_coloumn(table,column)
+# rename_column(table,column,new_name )
